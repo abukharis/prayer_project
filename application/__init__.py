8d128c59990e1578b3203128ff47f22051739496
abukharis <sa.abukharis@gmail.com>
2020-03-22 18:12:43 +0000

    sixth14 commit

--- application/__init__.py
+++ application/__init__.py
@@ -1,15 +1,15 @@
 # import Flask class from the flask module
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
-#from os import getenv
+
+from os import getenv
 #from flask_bcrypt import Bcrypt
 #from flask_login import LoginManager
 
 # create a new instance of Flask and store it in app 
 app = Flask(__name__)
 #bcrypt = Bcrypt(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
 DATABASE_URI="mysql+pymysql://root:salimd@35.242.160.164:3306/prayer"
 #app.config['SECRET_KEY'] = getenv('MY_SECRET_KEY')
 
